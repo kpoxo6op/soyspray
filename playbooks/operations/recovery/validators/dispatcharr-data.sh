f32dde2bc14ea06c72880c5b3305bca834d2287e
kpoxo6op <kpoxo6op@gmail.com>
2026-09-07 01:25:31 +1200

    Use the verified Dispatcharr PostgreSQL restore identity

--- playbooks/operations/recovery/validators/dispatcharr-data.sh
+++ playbooks/operations/recovery/validators/dispatcharr-data.sh
@@ -5,11 +5,5 @@ rm -f /data/db/postmaster.pid
 printf 'local all all trust\n' > /tmp/hba
 pg_ctl -D /data/db -l /tmp/postgres.log -o "-c listen_addresses='' -c unix_socket_directories=/tmp -c hba_file=/tmp/hba" -w start
 trap 'pg_ctl -D /data/db -m fast -w stop' EXIT
-for user in postgres dispatcharr; do
- if psql -h /tmp -U "$user" -d postgres -Atc 'SELECT count(*) FROM pg_database' >/tmp/count 2>/dev/null; then
-  cat /tmp/count
-  pg_amcheck -h /tmp -U "$user" --all --install-missing
-  exit 0
- fi
-done
-exit 1
+psql -h /tmp -U dispatch -d postgres -Atc 'SELECT count(*) FROM pg_database'
+pg_amcheck -h /tmp -U dispatch --all --install-missing
